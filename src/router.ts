897232efd060b5919456cc8553904b72493179a6
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-10-03 23:08:11 +0200

    router + middleware

---
+++ src/router.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Product routes
+ */
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product/", () => {});
+router.delete("/product/:id", () => {});
+
+/**
+ * Release routes
+ */
+router.get("/release", () => {});
+router.get("/release/:id", () => {});
+router.put("/release/:id", () => {});
+router.post("/release/", () => {});
+router.delete("/release/:id", () => {});
+
+/**
+ * Update routes
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update/", () => {});
+router.delete("/update/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   console.log("hello from express");
   res.status(200);
   res.json({ message: "hello from express" });
 });
+
+app.use("/api", router);
 
 export default app;
